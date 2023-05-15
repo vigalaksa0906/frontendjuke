4bb153f836fd511e9d6f3158494e0632af473bfa
djihan hardjanto <vigalucubanget@gmail.com>
2023-05-15 14:04:49 +0700

    pindah dari codesandbox

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout>
+
+  </NuxtLayout>
+</template>
+
+<style lang="postcss" scoped>
+.badge {
+  @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
+  &:hover {
+    @apply bg-gray-300;
+  }
+}
+</style>
