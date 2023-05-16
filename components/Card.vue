e2905c1a30dbae40b8111f5e0b591cd6621e1d10
djihan hardjanto <vigalucubanget@gmail.com>
2023-05-16 14:34:18 +0700

    update add card

---
+++ components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="drop-shadow mt-6 flex items-center justify-center">
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img
+        class="w-full"
+        src="https://i0.wp.com/resepkoki.id/wp-content/uploads/2017/01/Sunny-side-up.jpg?fit=500%2C333&ssl=1"
+        alt="Sunset in the mountains"
+      />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">{{ nama }}</div>
+        <p class="text-gray-700 text-base">{{ deskripsi }}</p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#masakanrumahan</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#simple</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#semuabisa</span
+        >
+      </div>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+export default {
+  props: {
+    nama: {
+      type: String,
+      default: "Nama masakan",
+    },
+    deskripsi: {
+      type: String,
+      default: "Deskripsi Makanan",
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,72 @@
 <template>
-  <NuxtLayout>
+  <!-- Some default layout shared across all pages -->
+  <div>
+    <section class="bg-gray-700 py-5">
+      <h2 class="text-center text-yellow-400 text-5xl font-bold">
+        Tutorial Membuat Telur Ceplok
+      </h2>
+    </section>
+    <div class="grid grid-flow-col text-center p-2">
+      <table class="border-Collapse border border-green-900">
+        <thead>
+          <tr>
+            <th class="border border-green-600">Alat & Bahan</th>
+            <th class="border border-green-600">Takaran</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="border border-green-600">Telur ayam</td>
+            <td class="border border-green-600">2 butir</td>
+          </tr>
+          <tr>
+            <td class="border border-green-600">Garam atau Masako</td>
+            <td class="border border-green-600">1/4 sendok teh</td>
+          </tr>
+          <tr>
+            <td class="border border-green-600">Minyak goreng</td>
+            <td class="border border-green-600">1 sendok makan</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <br />
+    <h2 class="mx-24">Berikut adalah tahapan membuat telur ceplok:</h2>
+    <div class="mx-24 bg-gray-200 text-justify px-6">
+      <br />
+      <ol class="list-decimal">
+        <li>Tuangkan minyak dalam wajan</li>
+        <li>Panaskan wajan</li>
+        <li>Pecahkan telur lalu taruh ke dalam wajan perlahan</li>
+        <li>Taburkan sedikit garam atau masako</li>
+        <li>Tunggu sebentar lalu balik telur</li>
+        <li>Setelah matang angkat telur dan sajikan dalam piring</li>
+      </ol>
+      <br />
+    </div>
 
-  </NuxtLayout>
+    <h3 class="text-center text-yellow-400 text-3xl font-bold">
+      Untuk lebih jelasnya bisa menonton video berikut ini:
+    </h3>
+    <div class="flex items-center justify-center">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/eTF457SjyVc"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <Card :nama="nama" :deskripsi="deskripsi"></Card>
+    <br />
+    <footer>
+      <p class="text-center">
+        Copyright &copy; 2023. Viga Laksa Hardjanto<br />
+      </p>
+    </footer>
+  </div>
 </template>
 
 <style lang="postcss" scoped>
@@ -12,3 +77,15 @@
   }
 }
 </style>
+
+<script setup>
+export default {
+  data() {
+    return {
+      nama: "Telur Ceplok",
+      deskripsi:
+        "Ini adalah tutorial membuat telur ceplok dengan resep termudah dan tentunya enak",
+    };
+  },
+};
+</script>
